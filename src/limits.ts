6b5007b2aeb07982d90f664d01ea9c207b748bb7
Swapnil Bari <swapnil@gomarble.ai>
2025-02-22 14:07:30 +0530

    update o3-mini

--- src/limits.ts
+++ src/limits.ts
@@ -15,6 +15,9 @@ export class TokenLimits {
     } else if (model === 'gpt-4') {
       this.maxTokens = 8000
       this.responseTokens = 2000
+    } else if (model === 'o3-mini') {
+      this.maxTokens = 8000
+      this.responseTokens = 2000
     } else {
       this.maxTokens = 4000
       this.responseTokens = 1000
